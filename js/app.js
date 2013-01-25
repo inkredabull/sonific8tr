41e95db75a07f2ce948538cb39b1b6deeab96a86
Anthony Bull <anthony@bluxomelabs.com>
2013-01-25 07:28:08 -0800

    Now working : most basic representation of app init with pre4
    Although trivial, represents the base case of getting a
    working Ember.js app.
    
    - removed deprecated routing and initialization code that is
      now (pre4) handled by the framework.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
 var App = Ember.Application.create();
 
-App.ApplicationController = Ember.Controller.extend();
-
-App.ApplicationView = Ember.View.extend({
-  templateName: 'application'
-});
-
-App.Router = Ember.Router.extend({
-  root: Ember.Route.extend({
-    index: Ember.Route.extend({
-      route: '/'
-    })
-  })
-});
-
 App.initialize();
