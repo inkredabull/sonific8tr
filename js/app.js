c649a5118354feaacdfb5edf35f051c577bcac03
Anthony Bull <anthony@bluxomelabs.com>
2013-01-25 08:55:49 -0800

    Somewhat crufty update for demo'ing how object change can propagate

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 var App = Ember.Application.create();
 
+App.user = Ember.Object.create({
+  name: 'World!'
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+  setupController: function(controller, user) {
+    controller.set('content', App.user);
+  }
+});
+
 App.initialize();
