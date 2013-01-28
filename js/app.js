ac0c3b2aab57ab6e9142475ae9e286893c8cf12f
Anthony Bull <anthony@crowdflower.com>
2013-01-28 08:22:37 -0800

    milestoning route corrections

--- js/app.js
+++ js/app.js
@@ -117,22 +117,22 @@ App.Track.FIXTURES = [
 ];
 
 window.setTimeout(function(){
-    var widgetIframe = document.getElementById('sc-widget'),
-        widget = SC.Widget(widgetIframe);
+    //var widgetIframe = document.getElementById('sc-widget'),
+        //widget = SC.Widget(widgetIframe);
 
-    widget.bind(SC.Widget.Events.READY, function() {
-      widget.bind(SC.Widget.Events.PLAY, function() {
-        // get information about currently playing sound
-        widget.getCurrentSound(function(currentSound) { 
-          //console.log('sound ' + currentSound.get('') + 'began to play');
-        });
-      });
-      // get current level of volume
-      widget.getVolume(function(volume) {
-        console.log('current volume value is ' + volume);
-      });
-      // set new volume level
-      widget.setVolume(50);
-      // get the value of the current position
-    });
+    //widget.bind(SC.Widget.Events.READY, function() {
+      //widget.bind(SC.Widget.Events.PLAY, function() {
+        //// get information about currently playing sound
+        //widget.getCurrentSound(function(currentSound) { 
+          ////console.log('sound ' + currentSound.get('') + 'began to play');
+        //});
+      //});
+      //// get current level of volume
+      //widget.getVolume(function(volume) {
+        //console.log('current volume value is ' + volume);
+      //});
+      //// set new volume level
+      //widget.setVolume(50);
+      //// get the value of the current position
+    //});
 }, 1000);
