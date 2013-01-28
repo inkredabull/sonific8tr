905fc3a2f74545db6e959f213766bd3102a4b733
Anthony Bull <anthony@crowdflower.com>
2013-01-27 16:46:58 -0800

    milestone : search textfield bound to controller

--- js/app.js
+++ js/app.js
@@ -1,13 +1,51 @@
 var App = Ember.Application.create();
 
-App.user = Ember.Object.create({
-  name: 'World!'
+//App.user = Ember.Object.create({
+  //name: 'World!'
+//});
+
+// Router
+App.Router.map(function() {
+  this.resource('search');
 });
 
-App.ApplicationRoute = Ember.Route.extend({
-  setupController: function(controller, user) {
-    controller.set('content', App.user);
+//App.SearchRoute = Ember.Route.extend();
+
+App.SearchView = Ember.TextField.extend({
+  valueBinding: 'App.SearchController.model',
+  insertNewline: function() {
+    var value = this.get('value');
+    //if (value) {
+      //App.productsController.search(value);
+    //}
   }
 });
+
+//App.ApplicationRoute = Ember.Route.extend({
+  //setupController: function(controller, user) {
+    //controller.set('content', App.user);
+  //}
+//});
+
+//App.SearchRoute = Ember.Route.extend({
+  //setupController: function(controller, user) {
+    //controller.set('content', App.user);
+  //}
+//});
+
+
+
+//App.SearchController = Ember.ObjectController.extend();
+
+//App.SearchView = Ember.TextField.extend({
+  //valueBinding: 'App.SearchController.content.value',
+  //insertNewline: function() {
+    //alert('Submitted: ' + App.controllerFor('search').get('value'));
+  //}
+//});
+
+//App.SearchValue = Em.Object.create({
+  //value: ''
+//});
 
 App.initialize();
